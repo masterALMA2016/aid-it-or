0e54029c76f1655b19468958a1ba0b6aad72a141
Dralagen <dralagen@dralagen.fr>
2014-11-24 16:49:03 +0100

    add archi of command

---
+++ src/main/scala/org/alma/gl/Copy.scala
@@ -0,0 +1,10 @@
+package org.alma.gl
+
+/**
+ * Created on 24/11/14.
+ *
+ * @author dralagen
+ */
+class Copy extends Command {
+
+}
---
+++ src/main/scala/org/alma/gl/Paste.scala
@@ -0,0 +1,10 @@
+package org.alma.gl
+
+/**
+ * Created on 24/11/14.
+ *
+ * @author dralagen
+ */
+class Paste extends Command {
+
+}
